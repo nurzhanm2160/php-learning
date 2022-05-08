974c04c003971cb838483c84cf30c46260311dcf
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-05-08 12:00:00 +0600

    loops: for, while, do while

---
+++ loops.php
@@ -0,0 +1,20 @@
+<?php
+    for($i = 0; $i < 10; $i++)
+    {
+        echo $i . '<br/>';
+    }
+
+    $counter = 0;
+    while($counter < 10)
+    {
+        $counter++;
+        echo "$counter </br>";
+    }
+
+    $c = 0;
+    do
+    {
+        $c++;
+        echo "$c </br>";
+    }
+    while ($c < 10);
