90b4bad7e3c64de8999a3ab0dd245016d0cdf2ab
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-05-10 12:00:00 +0600

    associative array

--- associative-array.php
+++ associative-array.php
@@ -1,1 +1,21 @@
 <?php
+    $words = array('green' => 'зеленный', 'red' => 'красный', 'blue' => 'синий', 'purple' => 'фиолетовый');
+    $words = ['green' => 'зеленный', 'red' => 'красный', 'blue' => 'синий', 'purple' => 'фиолетовый'];
+
+
+    $countries = ["Germany" => "Berlin", "France" => "Paris", "Spain" => "Madrid"];
+    echo $countries["Spain"];   // Madrid
+    echo "<br />";
+    $countries["Spain"] = "Barcelona";
+    echo $countries["Spain"];   // Barcelona
+
+
+    foreach ($words as $word)
+    {
+        echo "word: $word </br>";
+    }
+
+    $data = [1=> "Tom", "id132" => "Sam", 56 => "Bob"];
+    echo $data[1];  // Tom
+    echo "<br />";
+    echo $data["id132"];    // Sam
