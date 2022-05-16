a06fceb36f018b90655e3eccab814b897cfcbfd2
nurzhanm2160 <nurzhan.murzakhmet@gmail.com>
2022-05-16 12:00:00 +0600

    $_POST, form

---
+++ sending data to the server/form.php
@@ -0,0 +1,30 @@
+<?php ?>
+<html>
+    <head>
+        <title>Form</title>
+    </head>
+    <body>
+    <?php
+
+
+    $name = 'не определенно';
+    $age = 'не определенно';
+
+
+    if (isset($_POST['username'])) {
+        $name = $_POST['username'];
+    }
+
+    if (isset($_POST['age'])) {
+        $age = $_POST['age'];
+    }
+
+    echo "Имя: $name<br> Возраст: $age";
+    ?>
+        <form method="POST">
+            <p>Имя: <input name="username" type="text" /></p>
+            <p>Возраст: <input name="age" type="number" /></p>
+            <input type="submit" value="Отправить" />
+        </form>
+    </body>
+</html>
